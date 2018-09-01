ced6bb80ca71f9df101bdb0d829035a9c9eb0075
Ray Becoskie <ray@becoskie.com>
2018-09-01 10:27:13 -0500

    Added login logout and admin

---
+++ controllers/authcontroller.js
@@ -0,0 +1,19 @@
+var exports = (module.exports = {});
+
+exports.signup = function(req, res) {
+  res.render("signup");
+};
+
+exports.signin = function(req, res) {
+  res.render("signin");
+};
+
+exports.admin = function(req, res) {
+  res.render("admin");
+};
+
+exports.logout = function(req, res) {
+  req.session.destroy(function(err) {
+    res.redirect("/");
+  });
+};
