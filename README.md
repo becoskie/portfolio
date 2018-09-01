abe985fc30cb015a527a6dc31593ca641c3c9d2b
Ray Becoskie <ray@becoskie.com>
2018-09-01 08:47:37 -0500

    fresh start

--- README.md
+++
@@ -1,4 +0,0 @@
-# Group-Project-2 Save
-
-This folder contains all the stuff that is working.
-Before I pushed. If anything is wrong use this for compare.
--- server.js
+++ server.js
@@ -1,26 +1,17 @@
 require("dotenv").config();
 var express = require("express");
-var passport = require("passport");
-var session = require("express-session");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
+
 var db = require("./models");
 
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + "/public"));
-
-// For Passport
-
-app.use(
-  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
-); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
+app.use(express.static("public"));
 
 // Handlebars
 app.engine(
@@ -28,27 +19,12 @@ app.engine(
   exphbs({
     defaultLayout: "main"
   })
-);app.set("view engine", "handlebars");
-
-//Models
-var models = require("./models");
-//Sync Database
-models.sequelize
-  .sync()
-  .then(function() {
-    console.log("Nice! Database looks fine");
-  })
-  .catch(function(err) {
-    console.log(err, "Something went wrong with the Database Update!");
-  });
+);
+app.set("view engine", "handlebars");
 
 // Routes
-require("./routes/auth.js")(app, passport);
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
-
-//load passport strategies
-require("./config/passport/passport.js")(passport, models.user);
 
 var syncOptions = { force: false };
 
@@ -57,6 +33,7 @@ var syncOptions = { force: false };
 if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
+
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
